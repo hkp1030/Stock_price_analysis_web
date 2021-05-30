d614de67029217d3b9314c58d2d9c889f81ed1cb
hong <fya1030@naver.com>
2021-05-30 14:17:59 +0900

    데이터 관리 모듈 추가

--- stock/data/static_app.py
+++ stock/data/static_app.py
@@ -5,3 +5,6 @@ with open('./stock/res/stock_names.csv', mode='r') as file:
     stock_names = {rows[0][1:]: rows[1] for rows in reader}
 
 
+# 주식 코드의 한글 종목명을 가져옴
+def get_stock_name(code):
+    return stock_names[code]
--- stock/views.py
+++ stock/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 import requests
 import feedparser
-from stock.link_creon import LinkCreon
-from stock import static_app
+from stock.data.link_creon import LinkCreon
+from stock.data.static_app import get_stock_name
 import json
+from stock.data.networks import LSTMNetwork
+import numpy as np
 
 
 # 주식 상세페이지
@@ -11,23 +13,24 @@ def detail(request, stock_id):
     name = get_stock_name(stock_id)
     news = get_google_news(name)
 
-    creon = LinkCreon('D:/rltrader-master/venv32/Scripts/python.exe', 'stock/creon_minute.py')
-    stock = creon.get_stock_data(stock_id)
-    stock.reverse()
-    stock_json = json.dumps(stock)
+    #creon = LinkCreon('D:/rltrader-master/venv32/Scripts/python.exe', 'stock/creon_minute.py')
+    #stock = creon.get_stock_data(stock_id)
+    #stock.reverse()
+    #stock_json = json.dumps(stock)
 
-    contents = {'name': name, 'news': news, 'stock_json':stock_json}
+    #contents = {'name': name, 'news': news, 'stock_json':stock_json}
+    net = LSTMNetwork(input_dim=10, output_dim=2, num_steps=5)
+    pred = net.predict(np.random.normal(size=[1, 5, 10]))
+    pred = list(map(lambda x: int(x*100), pred))
+
+    contents = {'name': name, 'news': news, 'pred': pred}
 
     return render(request, "stock/detail.html", contents)
 
 
-# 주식 코드의 한글 종목명을 가져옴
-def get_stock_name(code):
-    return static_app.stock_names[code]
-
 # 구글 뉴스 가져오기
 def get_google_news(keyword, country='ko'):
-    URL = 'https://news.google.com/rss/search?q={}+when:1d'.format(keyword)
+    URL = 'https://news.google.com/rss/search?q={}+when:7d'.format(keyword)
     if country == 'en':
         URL += '&hl=en-NG&gl=NG&ceid=NG:en'
     elif country == 'ko':
